794f6b9eb934ccc68f8c9c0e059a3ccd1633dd22
Farzin Ebrahimi <Farzinebrahimi.info@gmail.com>
2025-12-25 23:00:43 +0330

    feat: -Add Upload test route on controller.

--- FMS.API/Controller/FileController.cs
+++ FMS.API/Controller/FileController.cs
@@ -6,5 +6,18 @@ namespace FMS.API.Controller;
 [Route("api/[controller]")]
 public class FileController :ControllerBase
 {
+    /// <summary>
+    /// Upload a new file (metadata only - storage not implemented yet
+    /// </summary>
+    /// <returns></returns>
+    [HttpPost]
+    public IActionResult UploadFile()
+    {
+        return Ok(new
+            {
+                Message = "File uploaded successfully",
+                At = DateTime.UtcNow
+            });
+    }
     
 }
