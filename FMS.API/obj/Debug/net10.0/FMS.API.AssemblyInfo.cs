b631a1bc533568b6e807200f5288af521bccaf2a
Farzin Ebrahimi <Farzinebrahimi.info@gmail.com>
2025-12-25 23:33:19 +0330

    Update Debug.net10

--- FMS.API/obj/Debug/net10.0/FMS.API.AssemblyInfo.cs
+++ FMS.API/obj/Debug/net10.0/FMS.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FMS.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f47f0b7c923ae475c5e98db9cdfe6634355d55d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc4d0ad2149611359591b0ab3eeef6d55aa906a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("FMS.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FMS.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
