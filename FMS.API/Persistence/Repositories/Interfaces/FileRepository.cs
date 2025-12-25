7853563f490e99b68e29242e5598a2158ddf8629
Farzin Ebrahimi <Farzinebrahimi.info@gmail.com>
2025-12-25 23:57:29 +0330

    feat: -init Interfaces for repository.

---
+++ FMS.API/Persistence/Repositories/Interfaces/FileRepository.cs
@@ -0,0 +1,14 @@
+﻿using FMS.API.Entities;
+
+namespace FMS.API.Persistence.Repositories.Interfaces;
+
+public interface IFileRepository
+{
+    Task<FileObject> AddSync(IFormFile file);
+    Task<IEnumerable<FileObject>> GetAllAsync();
+    //TODO GetByIdAsync
+    //TODO UpdateAsync
+    //TODO SoftDeleteAsync
+    //TODO SoftDeleteByRangeAsync
+    //TODO RestoreAsync
+}
