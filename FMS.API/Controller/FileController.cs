a89d3aefd90acb99a773e416899be5ee4b431915
Farzin Ebrahimi <Farzinebrahimi.info@gmail.com>
2025-12-25 23:09:56 +0330

    feat: -Update, delete, delete range and get files list added.

--- FMS.API/Controller/FileController.cs
+++ FMS.API/Controller/FileController.cs
@@ -30,7 +30,58 @@ public class FileController :ControllerBase
         return Ok(new
         {
             Id = id,
-            Message = "Not implemented yet",
+            Message = "file by Id found",
+        });
+    }
+    /// <summary>
+    /// Get all files list
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    public IActionResult GetFiles()
+    {
+        return Ok(new
+        {
+            Message = "All files list",
+        });
+    }
+    /// <summary>
+    /// Update a file by Id
+    /// </summary>
+    /// <returns></returns>
+    [HttpPatch("id:int")]
+    public IActionResult UpdateFile()
+    {
+        return Ok(new
+        {
+            Message = "File Updated Successfully",
+        });
+    }
+
+    /// <summary>
+    /// Delete a file by id
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    [HttpDelete("id:int")]
+    public IActionResult DeleteFileById(int id)
+    {
+        return Ok(new
+        {
+            Id = id,
+            Message = "File Deleted Successfully",
+        });
+    }
+    /// <summary>
+    /// Delete a file by range
+    /// </summary>
+    /// <returns></returns>
+    [HttpDelete]
+    public IActionResult DeleteByRange()
+    {
+        return Ok(new
+        {
+            Message = "Files Deleted Successfully",
         });
     }
     
