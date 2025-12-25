5d0783f9443b5f5de531405bb1025867b6d277b9
Farzin Ebrahimi <Farzinebrahimi.info@gmail.com>
2025-12-25 22:26:26 +0330

    feat:
    -Initialize db context.
    -create file object entity.
    -init the local db on docker.
    -Improve the the Service collection & Application Builder.

---
+++ FMS.API/Persistence/Context/FmsDbContext.cs
@@ -0,0 +1,16 @@
+﻿using FMS.API.Entities;
+using FMS.API.Persistence.EntityConfigurations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace FMS.API.Persistence.Context;
+
+public sealed class FmsDbContext(DbContextOptions<FmsDbContext> options) : DbContext(options)
+{
+    public DbSet<FileObject> FileObjects => Set<FileObject>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new FileObjectConfiguration());
+    }
+}
